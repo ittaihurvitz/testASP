c233209d2d31226c6c00f4a141561347b3834b5e
ittaihu <ittaihu@gmail.com>
2019-01-06 16:59:06 +0200

    Add project files.

---
+++ WebApp1/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApp1.Models;
+
+namespace WebApp1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Demo(Demo model)
+        {
+            ViewBag.Message = "My Demo";
+            model.Text1 = "This is text 1";
+            model.Text2 = "This is text 2";
+
+            return View(model);
+        }
+    }
+}
---
+++ WebApp1/Models/Demo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp1.Models
+{
+    public class Demo
+    {
+        public string Text1 { get; set; }
+        public string Text2 { get; set; }
+    }
+}
